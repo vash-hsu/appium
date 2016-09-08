e4bf7293aeb542cc7ff282414f69a62947f61168
vash-hsu <vash.hsu@gmail.com>
2016-09-08 21:07:03 +0800

    after restructuring, test is clearer and faster

--- 01_helloworld/test_helloworld.py
+++ 01_helloworld/test_helloworld.py
@@ -14,57 +14,53 @@ class HelloWorldTests(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.string4test = "@_@/ Hello World!"
-
-    def setUp(self):
-        desired_caps = {}
-        desired_caps['platformName'] = 'Android'
-        desired_caps['platformVersion'] = '6.0'
-        desired_caps['deviceName'] = 'Android Emulator'
-        desired_caps['app'] = PATH(
+        cls.button_id = {"upper": "radioButton_upper",
+                         "lower": "radioButton_lower"}
+        cls.desired_caps = {}
+        cls.desired_caps['platformName'] = 'Android'
+        cls.desired_caps['platformVersion'] = '6.0'
+        cls.desired_caps['deviceName'] = 'Android Emulator'
+        cls.desired_caps['app'] = PATH(
             './com.blogspot.rulesare.helloworld_radiobutton.apk'
         )
-        desired_caps['appPackage'] = 'com.blogspot.rulesare.helloworld_radiobutton'
-        desired_caps['appActivity'] = '.MainActivity'
-        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+        cls.desired_caps['appPackage'] = 'com.blogspot.rulesare.helloworld_radiobutton'
+        cls.desired_caps['appActivity'] = '.MainActivity'
+        cls.driver = webdriver.Remote('http://localhost:4723/wd/hub',
+                                      cls.desired_caps)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+    def setUp(self):
+        # type Hello World string into [editText]
+        self.element_txt = self.driver.find_element_by_id("editText")
+        if self.element_txt:
+            self.element_txt.clear() # clean
+            self.element_txt.send_keys(self.string4test) # type new string
 
     def tearDown(self):
-        self.driver.quit()
+        pass
+
+    def _click_button(self, button_name):
+        element_btn = self.driver.find_element_by_id(button_name)
+        if element_btn:
+            element_btn.click()
+
+    def _string_should_be(self, expected):
+        string_on_screen = self.element_txt.text
+        self.assertEqual(string_on_screen, expected)
 
     def test_add_string_and_make_uppercase(self):
         "FAST: type String, click UpperCase, get STRING back"
-        # type Hello World string into [editText]
-        element_txt = self.driver.find_element_by_id("editText")
-        if element_txt:
-            # clean
-            element_txt.clear()
-            # type new string
-            element_txt.send_keys(self.string4test)
-        # click [All Upper Case]
-        element_btn_upper = self.driver.find_element_by_id("radioButton_upper")
-        if element_btn_upper:
-            element_btn_upper.click()
-        # check result
-        string_on_screen = element_txt.text
-        expected_result = self.string4test.upper()
-        self.assertEqual(string_on_screen, expected_result)
+        self._click_button(self.button_id['upper']) # click [All Upper Case]
+        self._string_should_be(self.string4test.upper()) # check result
 
     def test_add_string_and_make_lowercase(self):
         "FAST: type String, click Lower, get string back"
-        # type Hello World string into [editText]
-        element_txt = self.driver.find_element_by_id("editText")
-        if element_txt:
-            # clean
-            element_txt.clear()
-            # type new string
-            element_txt.send_keys(self.string4test)
-        # click [All Upper Case]
-        element_btn_upper = self.driver.find_element_by_id("radioButton_lower")
-        if element_btn_upper:
-            element_btn_upper.click()
-        # check result
-        string_on_screen = element_txt.text
-        expected_result = self.string4test.lower()
-        self.assertEqual(string_on_screen, expected_result)
+        self._click_button(self.button_id['lower']) # click [All Lower Case]
+        self._string_should_be(self.string4test.lower()) # check result
+
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(HelloWorldTests)
